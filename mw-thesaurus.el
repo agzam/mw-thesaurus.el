20246e1d530069740d3f66c34e9548fc914e45aa
Ag Ibragimov <agzam.ibragimov@gmail.com>
2017-09-03 20:12:30 -0700

    Initial commit

---
+++ mw-thesaurus.el
@@ -0,0 +1,40 @@
+;;; mw-thesaurus.el --- Merriam-Webster Thesaurus -*- lexical-binding: t; -*-
+;;
+;; Filename: mw-thesaurus.el
+;; Description: Thesaurus look up through www.dictionaryapi.com.
+;; Author: Ag Ibragimov
+;; Maintainer: Ag Ibragimov (concat "agzam.ibragimov" "@" "gm" "ail" ".c" "om")
+;; Copyright (C) 2017  Ag Ibragimov
+
+;; Keywords: synonyms thesaurus dictionary
+;; Version: 0.0.1
+
+;;; Commentary:
+
+;; Thesaurus look up through www.dictionaryapi.com - Merriam-Webster online dictionary
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; For more than 150 years, in print and now online, Merriam-Webster has been America's leading and most-trusted provider of language information.
+;; Each month, Merriam-Webster web sites offer guidance to more than 40 million visitors. In print, publications include Merriam-Webster's Collegiate Dictionary (among the best-selling books in American history) and newly published dictionaries for English-language learners.
+;; All Merriam-Webster products and services are backed by the largest team of professional dictionary editors and writers in America, and one of the largest in the world.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+
+(provide 'mw-thesaurus)
